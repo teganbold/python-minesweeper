7d96986dcac880ec110fd0a0cb499dc14633cf1d
Tegan Bold <tegan.bold@gmail.com>
2024-05-27 21:06:38 -0400

    Initial commit, game working

---
+++ minesweeper.py
@@ -0,0 +1,155 @@
+import random
+import re
+
+class Board:
+    def __init__(self, dim_size, num_bombs):
+        self.dim_size = dim_size
+        self.num_bombs = num_bombs
+
+        #create the board
+        #hlper func
+        self.board = self.make_new_board()
+        self.assign_values_to_board()
+
+        self.dug = set()
+
+    def make_new_board(self):
+        # Generate a new board
+        board = [[None for _ in range(self.dim_size)] for _ in range(self.dim_size)]
+
+        # Plant the bombs
+        bombs_planted = 0
+        while bombs_planted < self.num_bombs:
+            bomb_location = random.randint(0, self.dim_size**2 - 1)
+            row = bomb_location // self.dim_size
+            col = bomb_location % self.dim_size
+
+            if board[row][col] == '*': # We already planted a bomb so keep going
+                continue
+            board[row][col] = '*'
+            bombs_planted += 1
+        
+        return board
+
+    def assign_values_to_board(self):
+        for r in range(self.dim_size):
+            for c in range(self.dim_size):
+                if self.board[r][c] == "*":
+                    continue
+                self.board[r][c] = self.get_num_neighboring_bombs(r, c)
+
+    def get_num_neighboring_bombs(self, row, col):
+        # let's iterate through each of the neighboring positions and sum number of bombs
+        # top left: (row-1, col-1)
+        # top middle: (row-1, col)
+        # top right: (row-1, col+1)
+        # left: (row, col-1)
+        # right: (row, col+1)
+        # bottom left: (row+1, col-1)
+        # bottom middle: (row+1, col)
+        # bottom right: (row+1, col+1)
+
+        num_neighboring_bombs = 0
+        for r in range(max(0, row-1), min(self.dim_size-1, row+1)+1):
+            for c in range(max(0, col-1), min(self.dim_size-1, col+1)+1):
+                if r == row and c == col:
+                    #original location, don't check
+                    continue
+                if self.board[r][c] == "*":
+                    num_neighboring_bombs += 1
+        return num_neighboring_bombs
+
+    def dig(self, row, col):
+        # dig at that location!
+        # return True if successful dig, False if bomb dug
+
+        # a couple of scenarios to consider:
+        # hit a bomb -> game over
+        # dig at a location with neighboring bombs -> finish dig
+        # dig at a location with no neighboring bombs -> recursively dig neighbors!
+
+        self.dug.add((row, col))
+
+        if self.board[row][col] == "*":
+            return False
+        elif self.board[row][col] > 0:
+            return True
+
+        for r in range(max(0, row-1), min(self.dim_size-1, row+1)+1):
+            for c in range(max(0, col-1), min(self.dim_size-1, col+1)+1):
+                if (r, c) in self.dug:
+                    continue
+                self.dig(r, c)
+        return True
+        
+    def __str__(self):
+        visible_board = [[None for _ in range(self.dim_size)] for _ in range(self.dim_size)]
+        for row in range(self.dim_size):
+            for col in range(self.dim_size):
+                if (row, col) in self.dug:
+                    visible_board[row][col] = str(self.board[row][col])
+                else:
+                    visible_board[row][col] = " "
+        
+        # put this together in a string
+        string_rep = ''
+        # get max column widths for printing
+        widths = []
+        for idx in range(self.dim_size):
+            columns = map(lambda x: x[idx], visible_board)
+            widths.append(
+                len(
+                    max(columns, key = len)
+                )
+            )
+
+        # print the csv strings
+        indices = [i for i in range(self.dim_size)]
+        indices_row = '   '
+        cells = []
+        for idx, col in enumerate(indices):
+            format = '%-' + str(widths[idx]) + "s"
+            cells.append(format % (col))
+        indices_row += '  '.join(cells)
+        indices_row += '  \n'
+        
+        for i in range(len(visible_board)):
+            row = visible_board[i]
+            string_rep += f'{i} |'
+            cells = []
+            for idx, col in enumerate(row):
+                format = '%-' + str(widths[idx]) + "s"
+                cells.append(format % (col))
+            string_rep += ' |'.join(cells)
+            string_rep += ' |\n'
+
+        str_len = int(len(string_rep) / self.dim_size)
+        string_rep = indices_row + '-'*str_len + '\n' + string_rep + '-'*str_len
+
+        return string_rep
+
+def play(dim_size = 10, num_bombs = 10):
+    board = Board(dim_size, num_bombs)
+    safe = True
+    while len(board.dug) < board.dim_size ** 2 - num_bombs:
+        print(board)
+        user_input = re.split(',(\\s)*', input("Where would you like to dig? Input as row,col: "))  # '0, 3'
+        row, col = int(user_input[0]), int(user_input[-1])
+        if row < 0 or row >= board.dim_size or col < 0 or col >= dim_size:
+            print("Invalid location. Please enter again.")
+            continue
+        
+        #If it's valid, we dig
+        safe = board.dig(row, col)
+        if not safe:
+            break #game over
+        
+    if safe:
+        print("Congratulations, you are victorious")
+    else:
+        print("Game Over")
+        board.dug = [(r, c ) for r in range(board.dim_size) for c in range(board.dim_size)]
+        print(board)
+
+if __name__ == '__main__':
+    play()
